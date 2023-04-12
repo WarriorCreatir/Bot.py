dea04401ee14c4d6d7b07cff8cf8bb08eea10858
WarriorCreatir <SEDNEVANDREY@GMAIL.COM>
2023-04-11 20:03:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import openai
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 token = "6234374883:AAFjSSTWzCgqbhxGxlBir4yRG4O9NeWA3Lg"
-
-openai_api_key = "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"
+openai_api_key = "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"
 
 bot = Bot(token)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler()
-async def send(message: types.Message):
+async def handle_message(message: types.Message):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" you:", " AI:"],
+        prompt="You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+        stop=["You:"],
     )
     await message.answer(response["choices"][0]["text"])
-    bot.start_polling(dp_updates=True)
+
+
+executor.start_polling(dp, skip_updates=True)
